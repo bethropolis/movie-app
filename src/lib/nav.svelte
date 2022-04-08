2393d83a6d4b26b53f3d06bffd481a8d7b4496c2
Bethuel <bethropolis@gmail.com>
2022-04-09 02:39:53 +0300

    gh-pages

---
+++ src/lib/nav.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    let isSearch = false;
+  function handleClick(){
+    isSearch = !isSearch;
+    search = "";
+  }
+  function handleSubmit(){
+   
+  }
+  export let search = "";
+  export let tab;
+</script>
+
+<nav class="transparent">
+    <div class="nav-wrapper">
+  {#if isSearch}
+  <form action="#" on:submit|preventDefault="{handleSubmit}">
+    <input
+      type="text"
+      bind:value={search}
+      class="white"
+      placeholder="search for movie..."
+    /></form>
+    <i class="material-icons"  on:click|preventDefault="{handleClick}" style="position: absolute; z-index: 3;cursor: pointer;  top: 0;right: 0;color: gray;">close</i>
+    <!-- content here -->
+  {:else}
+
+      <ul class="left">
+        <li><a href="./" on:click|preventDefault="{()=>{tab = 1}}"><i class="material-icons">cloud</i></a></li>
+      </ul>
+      <ul class="right">
+        <li><a href="#" on:click|preventDefault="{handleClick}"><i class="material-icons">search</i></a></li>
+      </ul>
+   
+  {/if}
+ </div>
+</nav>
+
+<!-- markup (zero or more items) goes here -->
+<style></style>
